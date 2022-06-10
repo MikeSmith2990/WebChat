269dff63ada06a425206bcc11d95297fdb0ade9b
Mike Smith <36375212+MikeSmith2990@users.noreply.github.com>
2022-06-09 22:48:40 -0400

    Commit initial code with basic chat functionality

---
+++ index.js
@@ -0,0 +1,51 @@
+(async function () {
+    console.log('init running...')
+    const ws = await connectToServer()
+
+    ws.onmessage = (webSocketMessage) => {
+        console.log('message received')
+        const messageBody = JSON.parse(webSocketMessage.data)
+        //add message to chat log
+        document.getElementById('messages').innerHTML = 
+        document.getElementById('messages').innerHTML + 
+        (`<div class='message' style="color: #${messageBody.color}">
+            ${new Date().toLocaleString().split(',')[1].trim()}: ${messageBody.text}
+        </div>`)
+    }
+
+    console.log('adding listener...')
+    //button click
+    document.getElementById('btn').addEventListener('click', (evt) => {
+        appendMessage(messageBody)
+    })
+    //enter key
+    document.addEventListener('keyup', (evt) => {
+        if(evt.keyCode == 13){
+            appendMessage(messageBody)
+        }
+    })
+    console.log('listener added!')
+    
+    //send message
+    function appendMessage(messageBody){
+        const messageBody = { text: document.getElementById('textInput').value }
+        ws.send(JSON.stringify(messageBody))
+        document.getElementById('textInput').value = ''
+        document.getElementById('textInput').focus()
+    }
+
+    async function connectToServer() {
+        console.log("attempting to connect to ws...")
+        const ws = new WebSocket('ws://127.0.0.1:7071/ws')
+        return new Promise((resolve, reject) => {
+            const timer = setInterval(() => {
+                if (ws.readyState === 1) {
+                    clearInterval(timer)
+                    resolve(ws)
+                    console.log("connected to ws!")
+                }
+            }, 10)
+        })
+    }
+    console.log('init complete!')
+})();
---
+++ main.js
@@ -0,0 +1,47 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 7071 });
+const clients = new Map();
+
+wss.on('connection', (ws) => {
+    const id = uuidv4();
+    console.log("connection made: " + id)
+    const color = Math.floor(Math.random() * 999999);
+    const metadata = { id, color };
+
+    clients.set(ws, metadata);
+
+    ws.on('message', (messageAsString) => {
+      const message = JSON.parse(messageAsString);
+      const metadata = clients.get(ws);
+
+      message.sender = metadata.id;
+      message.color = metadata.color;
+
+      console.log(message);
+      
+      //echo user messages
+      [...clients.keys()].forEach((client) => {
+        client.send(JSON.stringify(message));
+      });
+    });  
+
+    //join message
+    [...clients.keys()].forEach((client) => {
+      client.send(JSON.stringify({text: 'Someone joined!'}));
+    });
+});
+
+wss.on("close", (ws) => {
+  console.log("connection closed: " + ws.metadata.id)
+  clients.delete(ws);
+});
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+console.log("wss up");
