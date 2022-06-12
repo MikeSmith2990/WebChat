84b90983a23b2827f340bb1d87889e74fd75a147
Dylan Orgeron <dylanorgeron@users.noreply.github.com>
2022-06-12 13:34:22 -0500

    typescript

---
+++ client/scripts/index.ts
@@ -0,0 +1,3 @@
+import { WebSocketFactory } from "./websocket-handler";
+
+const conn = WebSocketFactory.start()
---
+++ client/scripts/websocket-handler.ts
@@ -0,0 +1,71 @@
+const websocketAddress = 'ws://127.0.0.1:7071/ws'
+
+export default class WebSocketHandler{
+
+    public ws: WebSocket
+
+    constructor () {
+    }
+
+    public async init(){
+        console.log('Initializing websocket...')
+        this.ws = await this.connectToServer()
+        this.initHandlers(this.ws)
+        console.log('init complete!')
+    }
+
+    private async connectToServer(): Promise<WebSocket> {
+        console.log(`Attempting to connect to websocket via ${websocketAddress}...`)
+        const ws = new WebSocket(websocketAddress)
+        return new Promise((resolve) => {
+            const timer = setInterval(() => {
+                if (ws.readyState === 1) {
+                    clearInterval(timer)
+                    resolve(ws)
+                    console.log("Connected to websocket!")
+                }
+            }, 10)
+        })
+    }
+
+    private initHandlers(ws: WebSocket): void{
+        ws.onmessage = (webSocketMessage) => {
+            console.log('message received')
+            const messageBody = JSON.parse(webSocketMessage.data)
+            //add message to chat log
+            document.getElementById('messages').innerHTML = 
+            document.getElementById('messages').innerHTML + 
+            (`<div class='message' style="color: #${messageBody.color}">
+                ${new Date().toLocaleString().split(',')[1].trim()}: ${messageBody.text}
+            </div>`)
+        }
+        console.log('adding listener...')
+        //button click
+        document.getElementById('btn').addEventListener('click', (evt) => {
+            this.appendMessage()
+        })
+        //enter key
+        document.addEventListener('keyup', (evt) => {
+            if(evt.keyCode == 13){
+                this.appendMessage()
+            }
+        })
+        console.log('listener added!')
+    }
+    
+    //send message
+    private appendMessage(): void{
+        const messageBody = { text: (document.getElementById('textInput') as HTMLInputElement).value }
+        this.ws.send(JSON.stringify(messageBody));
+        (document.getElementById('textInput') as HTMLInputElement).value = '';
+        document.getElementById('textInput').focus();
+    }
+}
+
+export class WebSocketFactory{
+    public static async start(){
+        const webSocketHandler = new WebSocketHandler();
+        webSocketHandler.init();
+        return webSocketHandler;
+    }
+}
